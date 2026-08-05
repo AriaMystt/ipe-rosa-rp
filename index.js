8ccf0f51e758aa8e5be07995613aaccea830ba10
AriaMyst <aria.jpeg@gmail.com>
2026-08-05 06:33:08 -0300

    fixed res.sendFile from 'host' to 'dist'

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 // 2. CHANGED: Added "splat" name to wildcard to prevent path-to-regexp v8 crash
 app.get('/{*splat}', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
