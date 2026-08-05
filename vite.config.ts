bc4b5363048ae8081bfc470c3fb03f9293966a37
AriaMyst <aria.jpeg@gmail.com>
2026-08-05 06:05:37 -0300

    please just work discloud

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/",
+  base: '/', // Fixes 404 path issues on production builds
+  preview: {
+    port: 8080,
+    host: '0.0.0.0'
+  }
 })
